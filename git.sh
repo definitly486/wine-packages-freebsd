702fcd5bc9d530c581a5185e71f3c5ac98725101
Your Name <you@example.com>
2025-02-13 16:15:48 +0300

    новый файл:    git.sh 	новый файл:    linux-binance-128.0.6613.189.pkg 	новый файл:    linux-github-desktop-3.4.9.pkg

---
+++ git.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+dir=$(dirname "$(realpath $0)")
+cd $dir
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+git add --all
+git add .
+git commit -n
+#git push https://github.com/xinitronix/mate.git
+git push https://github.com/xinitronix/wine-ports-freebsd.git
+
