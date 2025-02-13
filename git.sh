b89f0aceef324588e115dc6c8427ea6ebeaf6edd
Your Name <you@example.com>
2025-02-13 16:18:35 +0300

    изменено:      git.sh

--- git.sh
+++ git.sh
@@ -6,5 +6,5 @@ git config --global user.name "Your Name"
 git add --all
 git add .
 git commit -n
-git push https://github.com/definitly486/wine-ports-freebsd.git
+git push https://github.com/definitly486/wine-packages-freebsd
 
